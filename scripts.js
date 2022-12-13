1e13349ca6815ffe3fdd99cd488c573d1e3f8da7
Callum Gilfedder <Callumgilf@mail.com>
2022-12-13 16:00:10 +0000

    theme toggle working

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
-let themeToggle = document.getElementById("theme-toggle");
-themeToggle.addEventListener("click", toggleTheme);
-function toggleTheme () {
-    if (themeToggle.value == "OFF") {
-        themeToggle.value = "ON";
-      } else {
-        themeToggle.value = "OFF";
-      }
+let toggleButton = document.getElementById("toggle")
+
+toggleButton.addEventListener("click", toggleTheme)
+
+function toggleTheme() {
+    let page = document.getElementsByClassName("page")
+    for (let pages of page) {
+        pages.classList.toggle("page-light")
+    }
+    let altPage = document.getElementsByClassName("page-alt")
+    console.log(altPage)
+    for (let altPages of altPage) {
+        altPages.classList.toggle("page-alt-light")
+    };
+    let nav = document.getElementById("navbar") 
+    nav.classList.toggle("nav-light")
+    
 }
