ebc4668de623a431a3c26ea933aacd1dac874662
Callum Gilfedder <Callumgilf@mail.com>
2022-12-14 11:09:49 +0000

    Menu toggle bar in mobile view working perfectly now

--- scripts.js
+++ scripts.js
@@ -8,11 +8,35 @@ function toggleTheme() {
         pages.classList.toggle("page-light")
     }
     let altPage = document.getElementsByClassName("page-alt")
-    console.log(altPage)
     for (let altPages of altPage) {
         altPages.classList.toggle("page-alt-light")
     };
     let nav = document.getElementById("navbar") 
     nav.classList.toggle("nav-light")
-    
+
+    if (toggleButton.innerHTML === "Light Mode") {
+        toggleButton.innerHTML = "Dark Mode";
+    } else if (toggleButton.innerHTML = "Dark Mode") {
+        toggleButton.innerHTML = "Light Mode";
+    }    
+}
+
+
+let burger = document.getElementById("burger")
+burger.addEventListener("click", toggleMenu)
+let ul = document.getElementById("ul")
+ul.classList.toggle("ul-after")
+let nav = document.getElementById("navbar") 
+nav.classList.toggle("nav-after")
+
+function toggleMenu () {
+    if (burger.innerHTML == "☰ Expand Menu") {
+        burger.innerHTML = "☰ Collapse Menu";
+    } else if (burger.innerHTML = "☰ Collapse Menu") {
+        burger.innerHTML = "☰ Expand Menu";
+    }
+
+    ul.classList.toggle("ul-after")
+
+    nav.classList.toggle("nav-after")
 }
